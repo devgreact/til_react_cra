6093cfe5193bba8721542de49232c64788a9e096
devgreact <dev.greact@gmail.com>
2025-07-08 13:14:40 +0900

    [docs] jsx 기초

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 // css
 import "./index.css";
 import StartPage from "./pages/StartPage";
+import Posts from "./pages/Posts";
+import Albums from "./pages/Albums";
+import Test from "./pages/Test";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div className="wrap">
-    <StartPage></StartPage>
+    <Test></Test>
   </div>,
 );
