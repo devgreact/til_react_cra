636d14a2235da3423c3919e89860abe1ef22d114
devgreact <dev.greact@gmail.com>
2025-07-10 17:52:14 +0900

    [docs] 실전예제 진행중

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import StartPage from "./pages/StartPage";
 import Posts from "./pages/Posts";
 import Albums from "./pages/Albums";
 import Test from "./pages/Test";
+import RegisterPage from "./pages/RegisterPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div className="wrap">
-    <Test></Test>
+    <RegisterPage></RegisterPage>
   </div>,
 );
